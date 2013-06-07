ca80b87006cd66671eee178495298a50f05499b9
jun91367 <jun91367@gmail.com>
2013-06-07 15:05:09 +0800

    set up controller model

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,3 +1,9 @@
 class Comment < ActiveRecord::Base
   attr_accessible :body, :name
+  belongs_to :forum
+
+  default_scope order("create_at desc")
+
+  validates_presence_of :name
+  validates_presence_of :body
 end
